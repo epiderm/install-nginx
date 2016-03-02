04df31306e568b0e2e0098ce891fe72b8122ae1c
Mickael Lecoq <m.blacksad@gmail.com>
2016-03-02 14:14:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Install script of nginx
+
+This script downloads and installs nginx with ngx-pagespeed plugin.
+
+## Prerequisites 
+
+This script uses *wget*, *unzip*, *make* commands and *gcc* compiler.
+You have to install them if they are missing.
+
+## Command
+
+     ./install-nginx.sh
+
+
+## Next steps 
+
+Nginx configuration (see /etc/nginx/nginx.conf)
+
+
---
+++ install-nginx.sh
@@ -0,0 +1,25 @@
+cd
+NPS_VERSION=1.10.33.5
+wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip -O release-${NPS_VERSION}-beta.zip
+unzip release-${NPS_VERSION}-beta.zip
+cd ngx_pagespeed-release-${NPS_VERSION}-beta/
+wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
+tar -xzvf ${NPS_VERSION}.tar.gz  # extracts to psol/
+
+
+
+cd
+# check http://nginx.org/en/download.html for the latest version
+NGINX_VERSION=1.9.12
+wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+tar -xvzf nginx-${NGINX_VERSION}.tar.gz
+cd nginx-${NGINX_VERSION}/
+./configure --add-module=$HOME/ngx_pagespeed-release-${NPS_VERSION}-beta \
+  --prefix=/usr/local/share/nginx --conf-path=/etc/nginx/nginx.conf \
+  --sbin-path=/usr/local/sbin --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log
+
+make
+sudo make install
+
+
